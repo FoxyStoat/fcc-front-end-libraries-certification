ee7ca5f0737005cf20e25fff15239dd4b8ff8502
FoxyStoat <carol.hodgson@hotmail.com>
2020-12-01 20:30:15 +0000

    feat: Add const variable to hold url to JSON data

--- random quote machine/assets/scripts.js
+++ random quote machine/assets/scripts.js
@@ -1,3 +1,5 @@
 $ (document).ready(function() {
-
 });
+
+// My Quotes stored as JSON Data on Github
+const quoteData = "https://github.com/FoxyStoat/fcc-front-end-libraries-certification-/blob/main/random%20quote%20machine/assets/data.json";
