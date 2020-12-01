a5749f48323accd8347214e42bf25e47176262e5
FoxyStoat <carol.hodgson@hotmail.com>
2020-12-01 21:53:06 +0000

    feat: Change on click function to jquery syntax

--- random quote machine/assets/scripts.js
+++ random quote machine/assets/scripts.js
@@ -3,7 +3,12 @@ const quoteData = "https://github.com/FoxyStoat/fcc-front-end-libraries-certific
 
 $ (document).ready(function() {
   // Event listener for on click new quote button
-  $("#new-quote").on('click'), function() {
-
-  }
+  $('#new-quote').click(function() {
+    console.log("I was clicked");
+    // fetch quote data
+    // $.getJSON("https://github.com/FoxyStoat/fcc-front-end-libraries-certification-/blob/main/random%20quote%20machine/assets/data.json", function(result){
+      // $("#quote").html(result[0].text + '-' + result[0].author)
+      // console.log(result);
+    // });
+  });
 });
