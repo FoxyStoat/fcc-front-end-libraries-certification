96809a387b690a16bd8d07b01f02fa180ef3e5e4
FoxyStoat <carol.hodgson@hotmail.com>
2020-12-01 20:37:43 +0000

    feat: Add event listener for on click of new quote button

--- random quote machine/assets/scripts.js
+++ random quote machine/assets/scripts.js
@@ -1,5 +1,9 @@
-$ (document).ready(function() {
-});
-
 // My Quotes stored as JSON Data on Github
 const quoteData = "https://github.com/FoxyStoat/fcc-front-end-libraries-certification-/blob/main/random%20quote%20machine/assets/data.json";
+
+$ (document).ready(function() {
+  // Event listener for on click new quote button
+  $("#new-quote").on('click'), function() {
+
+  }
+});
