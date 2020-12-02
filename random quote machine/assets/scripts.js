a87d6412f8394fec411c58abcf452440aca56219
FoxyStoat <carol.hodgson@hotmail.com>
2020-12-02 20:13:40 +0000

    feat: Add get json request

--- random quote machine/assets/scripts.js
+++ random quote machine/assets/scripts.js
@@ -1,14 +1,13 @@
-// My Quotes stored as JSON Data on Github
-const quoteData = "https://github.com/FoxyStoat/fcc-front-end-libraries-certification-/blob/main/random%20quote%20machine/assets/data.json";
+// Quotes stored as JSON Data
+const quoteData = "assets/data.json";
 
 $ (document).ready(function() {
   // Event listener for on click new quote button
   $('#new-quote').click(function() {
     console.log("I was clicked");
     // fetch quote data
-    // $.getJSON("https://github.com/FoxyStoat/fcc-front-end-libraries-certification-/blob/main/random%20quote%20machine/assets/data.json", function(result){
-      // $("#quote").html(result[0].text + '-' + result[0].author)
-      // console.log(result);
-    // });
-  });
-});
+    $.getJSON(quoteData, function(result) {
+      console.log(result);
+    }); //get JSON end
+  }); //on click end
+}); //end document ready
