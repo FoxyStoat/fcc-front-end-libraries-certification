db045eb1e5eb95ce9d048318c2c553597884c28d
FoxyStoat <carol.hodgson@hotmail.com>
2020-12-03 18:48:59 +0000

    feat: Add Fisher-Yates shuffle function to get random quote

--- random quote machine/assets/scripts.js
+++ random quote machine/assets/scripts.js
@@ -1,13 +1,29 @@
 // Quotes stored as JSON Data
 const quoteData = "assets/data.json";
+let suffledQuotes = [];
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
 
 $ (document).ready(function() {
   // Event listener for on click new quote button
   $('#new-quote').click(function() {
-    console.log("I was clicked");
     // fetch quote data
     $.getJSON(quoteData, function(result) {
-      console.log(result);
+      // invoke shuffle function and store in variable shuffledData
+      shuffledQuotes = shuffle(result);
+      console.log(shuffledQuotes);
     }); //get JSON end
   }); //on click end
 }); //end document ready
