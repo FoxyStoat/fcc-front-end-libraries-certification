c4aabafe38299f34a287058345c6eded1e85e108
FoxyStoat <carol.hodgson@hotmail.com>
2020-12-05 19:48:22 +0000

    refactor: Move everything into document ready function

--- random quote machine/assets/scripts.js
+++ random quote machine/assets/scripts.js
@@ -1,9 +1,10 @@
-// Quotes stored as JSON Data
-const quoteData = "assets/data.json";
-let shuffledQuotes = [];
+$ (document).ready(function() {
+  // Quotes stored as JSON Data
+  const quoteData = "assets/data.json";
+  let shuffledQuotes = [];
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
+  // Shuffle function from http://stackoverflow.com/a/2450976
+  function shuffle(array) {
   let currentIndex = array.length, temporaryValue, randomIndex;
 
   while (currentIndex !== 0) {
@@ -14,32 +15,31 @@ function shuffle(array) {
       array[randomIndex] = temporaryValue;
   }
   return array;
-}
+  }
 
-function shuffleArray() {
-  // fetch quote data
-  $.getJSON(quoteData, function(result) {
-    // invoke shuffle function and store in variable shuffledData
-    shuffledQuotes = shuffle(result);
-    // call randomQuote function
-    randomQuote();
-  }); //get JSON end
-} //function end
+  function shuffleArray() {
+    // fetch quote data
+    $.getJSON(quoteData, function(result) {
+      // invoke shuffle function and store in variable shuffledData
+      shuffledQuotes = shuffle(result);
+      // call randomQuote function
+      randomQuote();
+    }); //get JSON end
+  } //function end
 
-function randomQuote() {
-  // iterate over shuffledQuotes array to update quote
-  $.each(shuffledQuotes, function(i, item) {
-    $('#text').html(item.quote);
-    $('#author').html("-" + " " + item.author);
-    $('#cite').html(item.cite);
-  }); //for each end
-} //function end
+  function randomQuote() {
+    // iterate over shuffledQuotes array to update quote
+    $.each(shuffledQuotes, function(i, item) {
+      $('#text').html(item.quote);
+      $('#author').html("-" + " " + item.author);
+      $('#cite').html(item.cite);
+    }); //for each end
+  } //function end
 
   // Event listener for on click new quote button
   $('#new-quote').click(function() {
     shuffleArray();
   }); //on click end
 
-$ (document).ready(function() {
   shuffleArray();
 }); //end document ready
