426e008a66df30c13fc11609270d6140729b61f0
FoxyStoat <carol.hodgson@hotmail.com>
2020-12-12 14:17:16 +0000

    refactor: Update to template literals in random quote funtion

--- Dark Souls random quote machine/assets/scripts.js
+++ Dark Souls random quote machine/assets/scripts.js
@@ -31,7 +31,7 @@ $ (document).ready(function() {
   function randomQuote() {
     // iterate over shuffledQuotes array to update quote
     $.each(shuffledQuotes, function(i, item) {
-      $('#text').html("<i class=\"fa fa-quote-left left-quote\" aria-hidden=\"true\"></i>" + " " + item.quote);
+      $('#text').html(`<i class="fa fa-quote-left left-quote" aria-hidden="true"></i> ${item.quote}`);
       $('#author').html(`- ${item.author}`);
       $('#img-container').html(`<img src="${item.img}" id="img" alt="image of ${item.author}"/>`);
       $('#cite').html(`${item.cite}`);
